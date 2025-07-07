8a293085cb5e96217c3565894b4fb2ba83b75fae
shravanipidurkar <shravani.pidurkar@mitaoe.ac.in>
2025-07-07 16:32:25 +0530

    added cors

--- server/server.js
+++ server/server.js
@@ -15,7 +15,17 @@ const customerAuthRoutes = require('./routes/cus_auth');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [
+  'https://super30-project-ob5u.vercel.app', // your frontend deployed domain
+  'http://localhost:3000'                    // for local testing
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
+
+// app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
