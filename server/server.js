df8a9b985f82e14b177bc26fa530b6392b9427bf
shravanipidurkar <shravani.pidurkar@mitaoe.ac.in>
2025-07-13 19:02:27 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -23,6 +23,7 @@ const allowedOrigins = [
 app.use(cors({
   origin: allowedOrigins,
   credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // ✅ Handle preflight (OPTIONS) requests
